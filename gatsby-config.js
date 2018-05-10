19f863dfb07192dc9489b66bae6fb02b0d44f23d
varsan <oleksandr.varanytsia@gmail.com>
2018-05-10 17:31:00 +0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,19 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118964513-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
